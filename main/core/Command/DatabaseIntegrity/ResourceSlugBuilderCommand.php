6e49fc28971885a9dc3bed337feb289a877c5059
Godfraind <nicolas.godfraind@gmail.com>
2019-08-07 14:36:24 +0200

    [CoreBundle] Slug generation command is now instantaneous. (#6052)

--- main/core/Command/DatabaseIntegrity/ResourceSlugBuilderCommand.php
+++ main/core/Command/DatabaseIntegrity/ResourceSlugBuilderCommand.php
@@ -2,15 +2,12 @@
 
 namespace Claroline\CoreBundle\Command\DatabaseIntegrity;
 
-use Claroline\CoreBundle\Entity\Resource\ResourceNode;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ResourceSlugBuilderCommand extends ContainerAwareCommand
 {
-    const BATCH_SIZE = 500;
-
     protected function configure()
     {
         $this->setName('claroline:resource:slug')
@@ -19,26 +16,15 @@ class ResourceSlugBuilderCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $om = $this->getContainer()->get('claroline.persistence.object_manager');
-        $total = $om->count(ResourceNode::class);
-        $output->writeln('Building resource slug ('.$total.')');
+        $this->conn = $this->getContainer()->get('doctrine.dbal.default_connection');
 
-        $offset = 0;
+        $sql = "
+             UPDATE claro_resource_node node set SLUG = CONCAT(SUBSTR(node.name,1,100) , '-', node.id)
+        ";
 
-        while ($offset < $total) {
-            $nodes = $om->getRepository(ResourceNode::class)->findBy([], [], self::BATCH_SIZE, $offset);
-
-            foreach ($nodes as $node) {
-                //rebuild slug
-                $node->setSlug(null);
-                $om->persist($node);
-                $output->writeln('Building slug for resource '.$node->getPathForDisplay());
-                ++$offset;
-            }
-
-            $output->writeln('Flush');
-            $om->flush();
-            $om->clear();
-        }
+        $output->writeln($sql);
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+        $output->writeln('Done !');
     }
 }
