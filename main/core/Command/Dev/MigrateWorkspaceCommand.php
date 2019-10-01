5ed15890e6e9897faad4b9577b65d69e22007b1c
Laurent Gruber <laurent.gruber@claroline.com>
2019-10-01 17:54:19 +0200

    FFNatation

--- main/core/Command/Dev/MigrateWorkspaceCommand.php
+++ main/core/Command/Dev/MigrateWorkspaceCommand.php
@@ -23,6 +23,7 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
 
 /**
  * Creates an user, optionaly with a specific role (default to simple user).
